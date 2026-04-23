3fa8da415e720fb9491edd60b6c7ddd1ec274d8c
Apoorva Agrawal <apoorva.agrawal@hashicorp.com>
2026-04-23 16:41:15 +0530

    Correction

--- main.tf
+++ main.tf
@@ -1,29 +1,22 @@
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "tf_policy_bucket" {
   bucket = "my-unique-bucket-name-for-testing"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
 }
 
-resource "aws_ebs_volume" "example" {
-  availability_zone = "us-west-2a"
+resource "aws_ebs_volume" "tf_policy_volume" {
+  availability_zone = "us-west-2"
   size              = 20
 
-  tags = {
-    Name = "HelloWorld"
-  }
 }
 
-resource "aws_db_instance" "default" {
+resource "aws_db_instance" "tf_policy_db" {
   allocated_storage    = 40
   db_name              = "mydb"
   engine               = "mysql"
   engine_version       = "8.0.28"
   instance_class       = "db.t3.small"
   username             = "myuser"
-  password             = "mypassword"
+  password             = "mypassword123213"
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 }
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 
 output "s3_bucket_id" {
   description = "The ID of the S3 bucket"
-  value       = aws_s3_bucket.b.id
+  value       = aws_s3_bucket.tf_policy_bucket.id
 }
 
 output "ebs_volume_id" {
   description = "The ID of the EBS volume"
-  value       = aws_ebs_volume.example.id
+  value       = aws_ebs_volume.tf_policy_volume
 }
 
 output "db_instance_address" {
   description = "The address of the DB instance"
-  value       = aws_db_instance.default.address
+  value       = aws_db_instance.tf_policy_db.address
 }
